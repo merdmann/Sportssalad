8eb46eb362721e0ac365868275bb328bba693268
DESKTOP-6ST7TKK\michaelerdmann <michael.erdmann@snafu.de>
2019-04-14 13:19:29 +0200

    A Tasre of responsive

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM Tree loaded')
 
+    let searchText = "";
     const _your_image_ = document.getElementById("your-image");
-    const _btn_search_ = document.getElementById("btn-search");
     const _search_text_ = document.getElementById("search-text");
 
-    
+
     // grvatar access to pictures based on http://en.gravatar.com/site/implement/ and using md5.
     function gravatar(email, option) {
         const result = "https://www.gravatar.com/avatar/" + md5(email.toLowerCase().trim()) + "?s=" + option;
@@ -44,65 +44,52 @@ document.addEventListener('DOMContentLoaded', function () {
 
         switch (pageTitle) {
             case "Settings":
-                update("google-id");
-                update("avatar-id");
-                
-                // install the handklers for butoms
-                let _btn_test_it_ = document.getElementById('btn-test-it');
-                _btn_test_it_.addEventListener("click", function () {
-                    LS.setItem("your-name", document.getElementById("your-name").value)
-                    console.log(LS.getItem("your-name"))
-                    console.log("click")
-                })
                 break;
-            case "Sports Salad":
-                _your_image_.setAttribute("src", gravatar("michael.erdmann@snafu.de", 120));
+            case "Sports Salad": // the main page
+                _your_image_.setAttribute("src", gravatar("michael.erdmann@snafu.de", 240));
                 fetchData("https://api.football-data.org/v2/competitions/CL/matches");
-                _your_image_.setAttribute("src", gravatar("Michael.erdmann@snafu.de"))
-                
-
-                _btn_search_.addEventListener("click", function () {
-                    
-                })
-
-                console.log(_your_image_.className);
-                FillTable("Summary-Table", data.matches);
-
-
-                // when logged in  in show your face on the screen.
-
-                break;
 
             default:
                 console.log("Default" + pageTitle);
         }
     }
 
+
     // this function will take a list of names sorted accoring to engagment of the person.
-    function FillTable(root, data) {
+    function FillTable(root, data, filter) {
+        let shownLines=0;
         let tbdy = document.getElementById(root);
-        if (tbdy == null) return;
-
-
+        if (tbdy == null) {
+            consoe.log(root + "not found.....")
+            return;
+        }
+   
+        // for all data
         data.forEach(function (item) {
             var tr = tbdy.insertRow(-1)
             var td = tr.insertCell(0)
 
-            td.appendChild(document.createTextNode(item.utcDate))
+            let line = item.getUTCDate + ' ' + item.homeTeam.name + ' ' + item.awayTeam.name;
+            if (line.includes(filter)) {
 
-            // missed votes
-            td = tr.insertCell(-1)
-            td.appendChild(document.createTextNode(item.homeTeam.name))
+                td.appendChild(document.createTextNode(' ' + item.utcDate))
 
-            // missed votes in %
-            td = tr.insertCell(-1)
-            td.appendChild(document.createTextNode(item.awayTeam.name))
+                // home team
+                td = tr.insertCell(-1)
+                td.appendChild(document.createTextNode(' ' + item.homeTeam.name))
 
-            tbdy.appendChild(tr)
+                // 
+                td = tr.insertCell(-1)
+                td.appendChild(document.createTextNode(' ' + item.awayTeam.name))
+                td.classList.add("last-col");
+
+                tbdy.appendChild(tr);
+                
+                if (shownLines++ % 18 == 0) {}
+                
+            }
         }) // end for each
     }
-
-
 
     // do the per page rendering of the received data
     function ProcessAndRender(data) {
@@ -110,16 +97,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
         console.log("ProcessAndRender nbr of data items: " + data.matches.length + " for " + pageTitle + ")");
 
-        FillTable("Summary-Table", data.matches);
-    
         switch (pageTitle) {
             case "Settings":
                 update("google-id");
                 update("avatar-id");
                 break;
-            case "Sport Salad":
-                _your_image_.setAttribute("src", gravatar("Michael.erdmann@snafu.de"))
 
+            case "Sports Salad":
+                _your_image_.setAttribute("src", gravatar("Michael.erdmann@snafu.de"))
+                FillTable("summary-table", data.matches, searchText);
+                
+                _search_text_.addEventListener('keypress', function (e) {
+                    console.log( searchText )
+                    searchText = _search_text_.value;
+                    document.getElementById("summary-table").innerHTML = "";
+                    FillTable( "summary-table", data.matches, searchText )
+                });
+                break;
         }
     }
 
@@ -139,7 +133,7 @@ document.addEventListener('DOMContentLoaded', function () {
             })
             .then(function (myJson) {
                 document.body.style.cursor = 'auto'
-                console.log(myJson)
+
                 ProcessAndRender(myJson)
             })
             .catch(err => console.log(err))
