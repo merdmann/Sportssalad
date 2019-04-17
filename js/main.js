188beca7afaf5039781392af8810333a24856037
DESKTOP-6ST7TKK\michaelerdmann <michael.erdmann@snafu.de>
2019-04-17 15:17:23 +0200

    Pagination added

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 'use strict'
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM Tree loaded')
-    let Teams = new Map()
+    let Teams = new Map();
     let searchText = "";
     const _your_image_ = document.getElementById("your-image");
     const _search_text_ = document.getElementById("search-text");
-    const MS = 1000  /* 1000 ms == 1 seconds */
-    const HR  = 60    /* 60 min /  hr */
-    const SEC = 60   /* 60 sec / min */
+    const MS = 1000 /* 1000 ms == 1 seconds */
+    const HR = 60 /* 60 min /  hr */
+    const SEC = 60 /* 60 sec / min */
     // grvatar access to pictures based on http://en.gravatar.com/site/implement/ and using md5.
     function gravatar(email, option) {
         const result = "https://www.gravatar.com/avatar/" + md5(email.toLowerCase().trim()) + "?s=" + option;
         console.log(result);
         return result;
+    }
+
+
+    function IsVsible(name) {
+        let item = document.getElementById(name);
+
+        return item.display == "none";
     }
     //
     // This function is expectd to update the settings page and to place th
@@ -58,62 +65,96 @@ document.addEventListener('DOMContentLoaded', function () {
     /*
      * calculae the winner team 
      */
-    function TheWinnerIs(item) {    
-        if( item.score.winner != "DRAW" ) {
-        let winner = item.score.winner == "AWAY_TEAM" ?            
-            item.awayTeam.name : item.homeTeam.name;
+    function TheWinnerIs(item) {
+        if (item.score.winner != "DRAW") {
+            let winner = item.score.winner == "AWAY_TEAM" ?
+                item.awayTeam.name : item.homeTeam.name;
             return winner;
         }
     }
+
+
     // this function will take a list of names sorted accoring to engagment of the person.
     function FillTable(root, data, filter) {
-        let shownLines=0;
+        let shownLines = 0;
         let tbdy = document.getElementById(root);
         if (tbdy == null) {
             consoe.log(root + " not found.")
             return;
         }
-                // for all data
-        data.forEach(function (item) {
+        const by_status = function (a, b) {
+          return b.status - a.status
+        }
+        
+        // for all data
+        data.sort(by_status).forEach(function (item) {
             var tr = tbdy.insertRow(-1)
             var td = tr.insertCell(0)
+            let lastState = item.status;
             console.log(item);
             // this implemnt the filter for a givem Teams or anyhing else
             let line = item.getUTCDate + ' ' + item.homeTeam.name + ' ' + item.awayTeam.name + "!" + TheWinnerIs(item);
-            if (line.includes(filter) || item.state == "SCHEDULED") {
+            if (line.includes(filter) /* || item.status == "SCHEDULED" */) {
                 let date = item.utcDate.split('T')
                 td.appendChild(document.createTextNode(' ' + item.startDate))
                 td.classList.add("last-col");
+                console.log(td.classList);
                 // home team
+                
                 td = tr.insertCell(-1)
+                
                 td.appendChild(document.createTextNode(' ' + item.homeTeam.name));
                 td = tr.insertCell(-1)
+    
                 td.appendChild(document.createTextNode(' ' + item.awayTeam.name));
-                if( item.score.winner != "DRAW" ) {
+    
+                if (item.score.winner != "DRAW") {
                     let winner = TheWinnerIs(item);
                     td = tr.insertCell(-1)
-                    td.appendChild(document.createTextNode( winner ))
+                    td.appendChild(document.createTextNode(winner))
                 }
                 td.classList.add("last-col");
+                
+                td.appendChild(document.createTextNode("Team Id:" + item.homeTeam.id  ));
+             //   Teams.get( item.homeTeam.id )
                 tbdy.appendChild(tr);
-                console.log(Teams[item.homeTeam.id]);
-                if (shownLines++ % 18 == 0) {}    
+            
+                console.log(Teams.get(item.homeTeam.id));
+                if ( lastState !== item.status) { 
+                    lastState = item.status;
+                    console.log( lastState );
+                    // paginatiomn will be put if the state is changing
+                    /* <li class="page-item"><a class="page-link" href="#">1</a></li>*/
+                    const li = document.createElement("li");
+                    li.classList.add("page-item");
+                    const a = document.createElement("a")
+                    console.log(item.status)
+                    
+                    li.appendChild(a);
+                    a.setAttribute("href", "#");
+                    
+                    a.innerHTML = lastState;
+                    
+                    tbdy.appendChild(li);
+                }
             }
         }) // end for each
     }
+
     // do the per page rendering of the received data
     function ProcessAndRender(data) {
         let pageTitle = document.title;
         switch (pageTitle) {
             case "flash":
-                console.log(data);
-                 data.teams.forEach( function(item) {
-                    console.log( item.id );
-                    
-                    Teams[ item.id ] = {
-                        venue:   item.venue,
+                console.log(data.teams);
+                Array.from(data.teams).forEach(function (item) {
+                    console.log(item.id);
+
+                    Teams.set(item.id, {
+                        venue: item.venue,
                         address: item.addess
-                	}
+                    })
+
                 })
                 break;
             case "Settings":
@@ -126,10 +167,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 _your_image_.setAttribute("src", gravatar("Michael.erdmann@snafu.de"))
                 FillTable("summary-table", data.matches, searchText);
                 _search_text_.addEventListener('keypress', function (e) {
-                    console.log( searchText )
+                    console.log(searchText)
                     searchText = _search_text_.value;
                     document.getElementById("summary-table").innerHTML = "";
-                    FillTable( "summary-table", data.matches, searchText )
+                    FillTable("summary-table", data.matches, searchText)
                 });
                 break;
         }
